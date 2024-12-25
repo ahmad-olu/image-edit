b401a17159e43507f10db11ff5d66829eeb7d33e
ahmad olukotun <ahmadolukotun@gmail.com>
2024-12-25 22:51:44 +0100

    re-arranged the ui, tested on android, added feature to toggle between og and edit.

--- src/app/components/appbar/appbar.component.ts
+++ src/app/components/appbar/appbar.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ImageService } from '../../services/image.service';
 
 @Component({
   selector: 'app-appbar',
   standalone: true,
   imports: [],
-  template: ` <div class="text-center">app-bar</div> `,
+  template: `
+    <div class="px-8 flex items-center justify-evenly space-x-4">
+      <button
+        class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-red-500 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+        (click)="imageService.importImage()"
+      >
+        Upload Image
+      </button>
+
+      <div class="text-center flex-1">Mini <mark>Image</mark> Editor</div>
+      <button
+        class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-red-500 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+        (click)="imageService.saveImage()"
+      >
+        Save Image
+      </button>
+    </div>
+  `,
   styles: ``,
 })
-export class AppbarComponent {}
+export class AppbarComponent {
+  imageService = inject(ImageService);
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -14,23 +14,7 @@ import { invoke } from '@tauri-apps/api/core';
   template: `
     <app-appbar />
     <div class="flex flex-col items-center p-4">
-      <div class="mb-2">
-        <button
-          class="px-4 py-2 bg-gray-600 text-white rounded hover:bg-blue-600"
-          (click)="imageService.saveImage()"
-        >
-          Save Image
-        </button>
-      </div>
       <div class="flex flex-col items-center p-4">
-        <div class="mb-2">
-          <button
-            class="px-4 py-2 bg-gray-600 text-white rounded hover:bg-blue-600"
-            (click)="imageService.importImage()"
-          >
-            Upload Image
-          </button>
-        </div>
         @if (processing() === true) {
           Processing...
         }
@@ -38,25 +22,50 @@ import { invoke } from '@tauri-apps/api/core';
         {{ dbg() }}
         <!-- {{ toggleAndDescription().resize.showDescription }} -->
         <div class="flex justify-center items-center w-full h-auto">
-          <div class="flex bg-gray-50 w-full md:w-auto">
-            <img
-              *ngIf="imageService.editedImagePath()"
-              [src]="imageService.editedImagePath()"
-              alt="Edited Image"
-              class="w-full h-96 md:w-auto md:h-96 object-scale-down border-r-4 rounded-[12px]"
-              [style.height]="'30rem'"
-            />
-          </div>
+          @if (showEditedImage() === true) {
+            <div
+              class="flex bg-gray-50 w-full md:w-auto transition-opacity duration-1000 "
+            >
+              <img
+                *ngIf="imageService.editedImagePath()"
+                [src]="imageService.editedImagePath()"
+                alt="Edited Image"
+                class="w-full h-96 md:w-auto md:h-96 object-scale-down border-r-4 rounded-[12px]"
+                [style.height]="'30rem'"
+              />
+            </div>
+          } @else {
+            <div
+              class="flex bg-gray-50 w-full md:w-auto transition-opacity duration-1000 "
+            >
+              <img
+                *ngIf="imageService.originalImagePath()"
+                [src]="imageService.originalImagePath()"
+                alt="Original Image"
+                class="w-full h-96 md:w-auto md:h-96 object-scale-down border-r-4 rounded-[12px]"
+                [style.height]="'30rem'"
+              />
+            </div>
+          }
         </div>
       </div>
-      <app-edit-bar />
-      <div class="mb-2">
+      <!-- Toggle Button -->
+      <div class="text-center mt-4">
         <button
           class="px-4 py-2 bg-gray-600 text-white rounded hover:bg-blue-600"
-          (click)="imageService.editImage()"
+          (click)="toggleImage()"
         >
-          Edit Image
+          Toggle Image
         </button>
+        <app-edit-bar />
+        <div class="mb-2">
+          <button
+            class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-red-500 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+            (click)="imageService.editImage()"
+          >
+            Edit Image
+          </button>
+        </div>
       </div>
     </div>
   `,
@@ -67,6 +76,11 @@ export class HomeComponent {
   //   originalImagePath = signal<string | null>(null);
   //   editedImagePath = signal<string | null>(null);
   processing = signal<boolean>(false);
+
+  showEditedImage = signal<boolean>(true);
+  toggleImage() {
+    this.showEditedImage.set(!this.showEditedImage());
+  }
 
   dbg = signal<string>('0');
 
